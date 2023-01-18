f8599ab2379b763ed5b7aa189930d94f85db0c2d
Simon <homer12345@gmail.com>
2023-01-18 15:09:41 -0800

    Refresh widget/fix openweathermap api call

--- js/options.js
+++ js/options.js
@@ -1,8 +1,9 @@
 // Location name to be displayed on the widget
-var location_name = "Fonsett Village";
+const location_name = "Fonsett Village";
 
 // openweathermap.org API for weather icon
 // see https://openweathermap.org/current for format and details
-var weatherKey = "";
-var weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
-var weatherCity = "";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+const weatherKey = "";
+const lat = "";
+const lon = "";
--- js/script.js
+++ js/script.js
@@ -1,102 +1,104 @@
 function update_time() {
-    var m = moment();
-    $('.time').html(m.format('HH:mm'));
+  var m = moment();
+  $(".time").html(m.format("HH:mm"));
 }
 
 function update_timeicon() {
-    $(".timeicon").removeClass("morning day evening night midnight dawn");
+  $(".timeicon").removeClass("morning day evening night midnight dawn");
 
-    var m = moment();
-    var hour = m.hour();
-    var tod = "morning";
+  var m = moment();
+  var hour = m.hour();
+  var tod = "morning";
 
-    if (hour < 5) {
-        tod = "midnight";
-    } else if (hour < 7) {
-        tod = "dawn";
-    } else if (hour < 10) {
-        tod = "morning";
-    } else if (hour < 17) {
-        tod = "day";
-    } else if (hour < 19) {
-        tod = "evening";
-    } else {
-        tod = "night"
-    }
+  if (hour < 5) {
+    tod = "midnight";
+  } else if (hour < 7) {
+    tod = "dawn";
+  } else if (hour < 10) {
+    tod = "morning";
+  } else if (hour < 17) {
+    tod = "day";
+  } else if (hour < 19) {
+    tod = "evening";
+  } else {
+    tod = "night";
+  }
 
-    $(".timeicon").addClass(tod);
+  $(".timeicon").addClass(tod);
 }
 
 function update_weather() {
-    if (weatherKey && weatherUrl && weatherCity) {
-        var wurl = weatherUrl + "?q=" + weatherCity + "&appid=" + weatherKey;
+  if (weatherKey && weatherUrl && lat && lon) {
+    const wurl = `${weatherUrl}?lat=${lat}&lon=${lon}&appid=${weatherKey}`;
 
-        $.ajax({
-            url: wurl,
-            dataType: "json",
-            error: function (xhr) {
-                var resp = JSON.parse(xhr.responseText);
-                console.log(resp.cod + ": " + resp.message);
-            },
-            success: function (data) {
-                var weather = "";
-                var cast = data["weather"][0];
-                var group = cast.main;
-                var id = cast.id;
+    $.ajax({
+      url: wurl,
+      dataType: "json",
+      error: function (xhr) {
+        var resp = JSON.parse(xhr.responseText);
+        console.log(resp.cod + ": " + resp.message);
+      },
+      success: function (data) {
+        let weather = "";
+        const cast = data["weather"][0];
+        const group = cast.main;
+        const id = cast.id;
 
-                if (group == "Clouds") {
-                    weather = "cloudy";
-                } else if (group == "Atmosphere") {
-                    weather = "fog";
+        if (group == "Clouds") {
+          weather = "cloudy";
+        } else if (group == "Atmosphere") {
+          weather = "fog";
 
-                    if (id == 781 || id == 731) {
-                        weather = "tornado";
-                    }
-                } else if (group == "Clear") {
-                    weather = "fair";
-                } else if (group == "Rain" || group == "Drizzle") {
-                    weather = "rain";
+          if (id == 781 || id == 731) {
+            weather = "tornado";
+          }
+        } else if (group == "Clear") {
+          weather = "fair";
+        } else if (group == "Rain" || group == "Drizzle") {
+          weather = "rain";
 
-                    if (id == 511) {
-                        weather = "freezingrain";
-                    }
-                } else if (group == "Thunderstorm") {
-                    weather = "thunderstorm";
+          if (id == 511) {
+            weather = "freezingrain";
+          }
+        } else if (group == "Thunderstorm") {
+          weather = "thunderstorm";
 
-                    if (id == 210) {
-                        weather = "lightning";
-                    }
+          if (id == 210) {
+            weather = "lightning";
+          }
 
-                    if (id == 212) {
-                        weather = "heavystorm";
-                    }
-                } else if (group == "Snow") {
-                    weather = "snow";
-                }
+          if (id == 212) {
+            weather = "heavystorm";
+          }
+        } else if (group == "Snow") {
+          weather = "snow";
+        }
 
-                if (weather) {
-                    $(".weathericon").removeClass().addClass("weathericon " + weather);
-                }
-            }
-        });
-    }
+        if (weather) {
+          $(".weathericon")
+            .removeClass()
+            .addClass("weathericon " + weather);
+        }
+      },
+    });
+  }
 }
 
 $(function () {
-    if (location_name) {
-        $(".location").text(location_name);
-    }
+  if (location_name) {
+    $(".location").text(location_name);
+  }
 
-    update_time();
-    update_timeicon();
+  update_time();
+  update_timeicon();
 
-    setInterval(update_time, 1000);
-    setInterval(update_timeicon, 3600000);
+  setInterval(update_time, 1000);
+  setInterval(update_timeicon, 3600000);
 
-    if (weatherKey && weatherUrl && weatherCity) {
-        update_weather();
-        setInterval(update_weather, 900000);
-    } else {
-        console.warn("xc2-time: Weather options not set!");
-    }
+  if (weatherKey && weatherUrl && lat && lon) {
+    update_weather();
+    setInterval(update_weather, 900000);
+  } else {
+    console.warn("xc2-time: Weather options not set!");
+  }
 });
